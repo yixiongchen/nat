59918038c5382070e827648123b3aff200312e07
yixiongchen <jack.chenyixiong@gmail.com>
2016-12-02 14:37:27 -0500

    Updated condition for handling packet for me

--- sr_router.c
+++ sr_router.c
@@ -17,6 +17,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "sr_if.h"
 #include "sr_rt.h"
@@ -411,7 +412,7 @@ int sr_handle_ip_pkt(struct sr_instance* sr,
   sr_ip_hdr_t *ip_hdr;
   sr_icmp_t8_hdr_t *icmp_t8_hdr;
   sr_tcp_hdr_t *tcp_hdr;
-  struct sr_nat_mapping *nat_mapping;
+  struct sr_nat_mapping *nat_mapping = NULL;
 
   ip_hdr = (sr_ip_hdr_t *)(packet + sizeof(struct sr_ethernet_hdr));
   assert(ip_hdr); 
@@ -424,27 +425,38 @@ int sr_handle_ip_pkt(struct sr_instance* sr,
 
   /* Look for nat mapping for corresponding dst_ip and dst_aux. */
   /* if the ip packet is an icmp packet */
-  if (ip_hdr->ip_p == ip_protocol_icmp) {
-    icmp_t8_hdr = (sr_icmp_t8_hdr_t *)(packet + sizeof(struct sr_ethernet_hdr) + 
-    sizeof(struct sr_ip_hdr));
-    
-    nat_mapping = sr_nat_lookup_external(sr->nat, icmp_t8_hdr->icmp_id, nat_mapping_icmp, 0, 0, 0, 0, 0);
-  }
-
-  /* if the ip packet is a tcp packet */
-  if (ip_hdr->ip_p == ip_protocol_tcp) {
-    tcp_hdr = (sr_tcp_hdr_t *)(packet + sizeof(struct sr_ethernet_hdr) + 
+  if (sr->nat_on){
+    if (ip_hdr->ip_p == ip_protocol_icmp) {
+      icmp_t8_hdr = (sr_icmp_t8_hdr_t *)(packet + sizeof(struct sr_ethernet_hdr) + 
       sizeof(struct sr_ip_hdr));
+      /* If the packet comes from outside. */
+      if (ip_hdr->ip_dst == sr->nat->ip_ext){
+        nat_mapping = sr_nat_lookup_external(sr->nat, icmp_t8_hdr->icmp_id, nat_mapping_icmp, 0, 0, 0, 0, 0);
+      }
+    }
 
-    uint8_t flag = tcp_hdr->flag;
-    int ack = flag & (1<<4);
-    int syn = flag & (1<<1);          
-    int fin = flag & 1;          
+    /* if the ip packet is a tcp packet */
+    if (ip_hdr->ip_p == ip_protocol_tcp) {
+      tcp_hdr = (sr_tcp_hdr_t *)(packet + sizeof(struct sr_ethernet_hdr) + 
+        sizeof(struct sr_ip_hdr));
 
-    nat_mapping = sr_nat_lookup_external(sr->nat, tcp_hdr->port_dst, nat_mapping_tcp, 
-      ip_hdr->ip_src, tcp_hdr->port_src, ack, syn, fin);
-  }  
-  
+      uint8_t flag = tcp_hdr->flag;
+      int ack = flag & (1<<4);
+      int syn = flag & (1<<1);          
+      int fin = flag & 1;          
+
+      /* If the packet comes from outside.*/
+      if (ip_hdr->ip_dst == sr->nat->ip_ext){
+        nat_mapping = sr_nat_lookup_external(sr->nat, tcp_hdr->port_dst, nat_mapping_tcp, 
+          ip_hdr->ip_src, tcp_hdr->port_src, ack, syn, fin);
+        if (!nat_mapping){
+          sleep(6);
+          nat_mapping = sr_nat_lookup_external(sr->nat, tcp_hdr->port_dst, nat_mapping_tcp, 
+            ip_hdr->ip_src, tcp_hdr->port_src, ack, syn, fin);
+        }
+      }
+    }
+  }
   struct sr_if* iface = sr_get_interface(sr, interface);
   assert(iface);
 
